2ac729f1af285f09ab500ec660d875ef24d6cdd8
Cheryl <jane31023@gmail.com>
2025-08-24 13:48:51 +0800

    更新動態

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-import './assets/scss/all.scss';
-import 'bootstrap/dist/js/bootstrap.min.js';
+// 動畫部分
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-console.log('Hello world');
+AOS.init({
+  duration: 700, // 動畫時間
+  once: false, // 設成 false，每次進入 viewport 都會重新觸發
+});
+// 動畫部分
+
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+console.log("Hello world");
